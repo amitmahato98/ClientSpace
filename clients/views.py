f440f4cf511eaf9602dc4cd4846034029d7a7a64
ANMOL4355 <anmolojha543@gmail.com>
2026-08-25 13:46:17 +0545

    users and other relevant details added

--- clients/views.py
+++ clients/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Client
 
 def clients_page(request):
-    return render(request, 'clients/clients.html')
+    clients = Client.objects.all()
+    selected = clients.first()
+
+    client_id = request.GET.get('client')
+    if client_id:
+        selected = get_object_or_404(Client, id=client_id)
+
+    return render(request, 'clients/clients.html', {
+        'clients': clients,
+        'selected': selected,
+    })
